a38eeaecadf1b337180441aa1a745bad93373f6b
gabrielsong9674 <gracegabrielson@icloud.com>
2020-03-10 14:42:52 -0700

    moreAlgorithms

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _02_More_Algorithms;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 public class Algorithms {
@@ -58,13 +59,59 @@ public class Algorithms {
 	
 	public static boolean containsSOS(List<String> message)
 	{
-		String SOS = " ... --- ...";
+		String SOS = " ... --- ... ";
 		for(int i = 0; i < message.size(); i ++)
 		{
-			System.out.println(message.get(i));
-			System.out.println(SOS);
+			if(SOS.equals(message.get(i)))
+			{
+				return true;
+			}
 		}
 		return false;
 		
+	}
+	
+	public static List<Double> sortScores(List<Double> results)
+	{
+		double temp;
+		for(int i = 0; i < results.size() - 1; i ++)
+		{
+		for(int j = 0; j < results.size() - i - 1; j ++)
+		{
+			if(results.get(j) > results.get(j+1))
+			{
+				temp = results.get(j);
+				results.set(j, results.get(j+1));
+				results.set(j+1, temp);
+			}
+		}
+	
+		}
+		return results;
+	}
+	
+	public static List<String> sortDNA(List<String> sequence)
+	{
+		String temp;
+		for(int i = 0; i < sequence.size(); i ++)
+		{
+			for(int j = 0; j < sequence.size()-i-1; j ++)
+			{
+				if(sequence.get(j).length() > sequence.get(j+1).length())
+				{
+					temp = sequence.get(j);
+					sequence.set(j, sequence.get(j+1));
+					sequence.set(j+1, temp);
+				}
+			}
+		}
+		System.out.println(sequence.get(0));
+		return sequence;
+	}
+	
+	public static List<String> sortWords(List<String> words)
+	{
+		
+		return words;
 	}
 }
